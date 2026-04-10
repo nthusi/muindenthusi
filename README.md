056948c9d161edc11722303a21b0493a02f31924
nthusi <muindenthusi@gmail.com>
2026-04-10 12:28:35 +0300

    final

---
+++ README.md
@@ -0,0 +1,7 @@
+A personal blog built using wagtail v6.4.2 and python 3.13's virtual environment.
+
+Steps on deploying to AWS through EC2 or docker:
+EC2
+
+Docker
+
---
+++ muindenthusi/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1(00u8ww+hs!ezbfi(y%83*u2mn4b&5sh8=k@ql&$f#*m@ud3!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
